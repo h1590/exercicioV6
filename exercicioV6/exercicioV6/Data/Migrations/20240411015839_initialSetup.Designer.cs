a6d77627ebf63f44060ef1b2fbad1d7717bad193
h1590 <te45henrique@hotmail.com>
2024-04-11 19:31:26 +0100

    Hidden isDeleted from page displays
    Hidden all references of "isDeleted" property in generated cshtml page views

--- exercicioV6/exercicioV6/Data/Migrations/20240411015839_initialSetup.Designer.cs
+++ exercicioV6/exercicioV6/Data/Migrations/20240411015839_initialSetup.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using exercicioV6.Data;
 
@@ -11,9 +12,10 @@ using exercicioV6.Data;
 namespace exercicioV6.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240411015839_initialSetup")]
+    partial class initialSetup
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ exercicioV6/exercicioV6/Data/Migrations/20240411015839_initialSetup.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace exercicioV6.Data.Migrations
+{
+    public partial class initialSetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contact",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    isDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    phoneNumber = table.Column<string>(type: "nvarchar(9)", maxLength: 9, nullable: false),
+                    email = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contact", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contact");
+        }
+    }
+}
